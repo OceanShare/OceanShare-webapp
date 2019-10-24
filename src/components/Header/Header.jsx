67bb37a466fb034f1b140646108ae479bbfe2b27
Guillaume Sagot <guillaume.sagot@epitech.eu>
2019-10-24 11:45:38 +0200

    Update views and add markers

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import { PropTypes } from "prop-types";
+import { NavLink as NavLinkRRD, Link } from "react-router-dom";
 
 import {
-  Form,
+  Button,
   Container,
   DropdownMenu,
   DropdownItem,
   DropdownToggle,
+  Form,
   Media,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
   Navbar,
+  NavItem,
+  NavLink,
   Nav,
   UncontrolledDropdown
 } from "reactstrap";
+import i18next from "i18next";
 
 import FRflag from "../../assets/locales/fr/fr.svg";
 import ENflag from "../../assets/locales/en/en.svg";
+import logo from "../../assets/logo-light.png";
 
 import { createBrowserHistory } from "history";
+import { Drawer, Icon } from "antd";
 const history = createBrowserHistory();
 
 class Header extends React.Component {
@@ -29,7 +40,10 @@ class Header extends React.Component {
       collapseOpen: false,
       color: "white",
       currentLng: "",
-      changeLng: ""
+      changeLng: "",
+      visible: false,
+      placement: "left",
+      color: "white",
     };
   }
 
@@ -50,13 +64,53 @@ class Header extends React.Component {
     }
   };
 
+  toggle = () => {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  };
+
+  showDrawer = () => {
+    this.setState({
+      visible: true
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false
+    });
+  };
+
+  onChange = e => {
+    this.setState({
+      placement: e.target.value
+    });
+  };
+
   onClickLogout() {
     localStorage.clear();
     history.push("/");
     window.location.reload();
   }
 
+  createLinks = routes => {
+    return routes.map((prop, key) => {
+      if (prop.invisible) return null;
+
+      return (
+        <NavItem key={key}>
+          <NavLink to={prop.path} tag={NavLinkRRD} onClick={this.onClose}>
+            <i className={"tim-icons " + prop.icon} />
+            {i18next.t(prop.name)}
+          </NavLink>
+        </NavItem>
+      );
+    });
+  };
+
   render() {
+    const { routes } = this.props
     return (
       <>
         <Navbar
@@ -66,16 +120,77 @@ class Header extends React.Component {
           style={{ padding: "0" }}
         >
           <Container fluid>
-            <Form className="navbar-search navbar-search-dark form-inline">
-              </Form>
+            <Drawer
+              title="Menu"
+              placement={this.state.placement}
+              closable={false}
+              onClose={this.onClose}
+              visible={this.state.visible}
+            >
+              <Navbar
+                className="navbar-vertical fixed-left navbar-light bg-white"
+                expand="md"
+                id="sidenav-main"
+              >
+                {/* <ListGroup>{this.createLinks(this.props.routes)}</ListGroup> */}
+                <Nav navbar>{this.createLinks(routes)}</Nav>
+                <Nav className="mt-md-9 px-3" navbar>
+                  <Container fluid>
+                    <Button
+                      size="xs"
+                      color="secondary"
+                      type="button" 
+                      className="btn btn-round block"
+                      style={{
+                        position: "absolute",
+                        bottom: "15px",
+                        width: "85%",
+                        left: "18px"
+                      }}
+                      onClick={this.toggle}
+                    >
+                      <i
+                        style={{ color: "red", cursor: "pointer" }}
+                        className={"tim-icons icon-button-power"}
+                      />{" "}
+                      Settings
+                </Button>
+                  </Container>
+                </Nav>
+              </Navbar>
+            </Drawer>
 
+            <Form className="navbar-search navbar-search-dark form-inline">
+              <UncontrolledDropdown nav>
+                <DropdownToggle className="pr-0" nav onClick={this.showDrawer}>
+                  <Media className="align-items-center">
+                    <Media className="ml-2 d-none d-lg-block">
+                      <span
+                        style={{ background: "#009fe3" }}
+                        className="avatar avatar-sm rounded-circle"
+                      >
+                        <Icon type="menu" />
+                      </span>
+                    </Media>
+                  </Media>
+                </DropdownToggle>
+              </UncontrolledDropdown>
+              <Link to={"/"} className="ml-3">
+                <img
+                  className="navbar-brand-img"
+                  alt={"logo"}
+                  src={logo}
+                  style={{ width: "150px" }}
+                />
+              </Link>
+            </Form>
             <Nav className="align-items-center d-none d-md-flex" navbar>
               <UncontrolledDropdown nav>
                 <DropdownToggle className="pr-0" nav>
                   <Media className="align-items-center">
                     <Media className="ml-2 d-none d-lg-block">
                       <span
-                        style={{ background: "#009fe3" }}
+                        style={{ background: "#f5365c" }}
                         className="avatar avatar-sm rounded-circle"
                       >
                         <i
@@ -95,7 +210,7 @@ class Header extends React.Component {
                     style={{ cursor: "pointer" }}
                     onClick={() => this.changeLanguage()}
                   >
-                    <i style={{ cursor: "pointer" }} />{ this.state.changeLng === "English" ?  <img src={ENflag} className="tim-icons" style={{width:"20px"}} alt="" /> :  <img src={FRflag}  className="tim-icons"style={{width:"20px"}} alt="" /> } {this.state.changeLng}
+                    <i style={{ cursor: "pointer" }} />{this.state.changeLng === "English" ? <img src={ENflag} className="tim-icons" style={{ width: "20px" }} alt="" /> : <img src={FRflag} className="tim-icons" style={{ width: "20px" }} alt="" />} {this.state.changeLng}
                   </DropdownItem>
 
                   <DropdownItem
